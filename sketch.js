f61b8505aeaebd80ee82ca574e6a5bf75abaacbf
Aditya1234Arya <91328208+Aditya1234Arya@users.noreply.github.com>
2021-09-24 21:26:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var box;
+
+box.shapecolor("blue")
+function draw() 
+{
+  background("yellow");
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+  background("red")=background("red");
+  }
+
+  drawSprites();
+}
+
+
+
+
